1af7eeed9d860a398e3d9639b78df6c6510d1ba5
ttai2023 <territ2023@student.cis.edu.hk>
2023-03-05 22:59:56 +0800

    Final Song

--- src/Song.java
+++ src/Song.java
@@ -1,4 +1,48 @@
-public class Song {
+import java.util.ArrayList;
 
-    private String
+public class Song {
+    private String title;
+    private String artist;
+    private ArrayList<String> listeners = new ArrayList<>();
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getArtist() {
+        return artist;
+    }
+
+    public void setArtist(String artist) {
+        this.artist = artist;
+    }
+
+    public int howMany(ArrayList<String> people) {
+        int newFans = 0;
+        for(String person: people) {
+            String update = person.toLowerCase();
+            if (!listeners.contains(update)) {
+                listeners.add(update);
+                newFans++;
+            }
+        }
+        return newFans;
+    }
+
+    public Song(String title, String artist) {
+        this.title = title;
+        this.artist = artist;
+    }
+
+    @Override
+    public String toString() {
+        return "Song{" +
+                "title='" + title + '\'' +
+                ", artist='" + artist + '\'' +
+                '}';
+    }
 }
